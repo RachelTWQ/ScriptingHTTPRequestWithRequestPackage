b7c9c41579c922951efdb4ad07e2bc0b38cc17cd
RachelTWQ <racheltang15@hotmail.com>
2019-04-08 18:43:47 +0000

    chain function

--- request.js
+++ request.js
@@ -1,6 +1,11 @@
 var request = require('request');
+var fs = require('fs');
 
-request('https://sytantris.github.io/http-examples/', function(err, response, body) {
-  if (err) throw err;
-  console.log('Response Status Code:', response.statusCode);
-});
+request.get('https://sytantris.github.io/http-examples/')
+    .on('error', function (err) {
+        throw err;
+    })
+    .on('response', function (response) {
+        console.log('Response Status Code: ', response.statusCode);
+    })
+    .pipe(fs.createWriteStream('./downloaded.html'))
